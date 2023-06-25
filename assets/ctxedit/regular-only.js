25be6de5090eb81d2fb61966ab707b7745ef1337
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2023-06-25 20:51:36 +0000

    Built site for gh-pages

--- assets/ctxedit/regular-only.js
+++ assets/ctxedit/regular-only.js
@@ -1,12 +1,13 @@
 // Replace all font-weight: 700 samples with font-weight: 400
 // because some fonts do not have a bold version
 window.onload = function () {
-  let elements = document.getElementsByTagName("livesample");
-  for (let i = 0; i < elements.length; i++) {
-    let style = elements[i].getAttribute("style");
-    if (style && /font-weight:\s*700;/.test(style)) {
-      let newStyle = style.replace(/(font-weight:\s*)700;/, "$1400;");
-      elements[i].setAttribute("style", newStyle);
+  var liveSampleElements = document.querySelectorAll("livesample");
+  liveSampleElements.forEach(function (element) {
+    if (
+      window.getComputedStyle(element, null).getPropertyValue("font-weight") ===
+      "700"
+    ) {
+      element.style.fontWeight = "400";
     }
-  }
+  });
 };
